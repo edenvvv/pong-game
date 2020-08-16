cb9e84b54623b0e587c7090c7ee9b849bdcbc919
edenvvv <edenvvv@gmail.com>
2020-08-16 22:20:46 +0300

    Final fixs

--- pong_game.py
+++ pong_game.py
@@ -5,7 +5,7 @@ import os
 
 wn = turtle.Screen()
 wn.title("Pong")
-wn.bgcolor("black")
+wn.bgcolor("#262626")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
@@ -40,8 +40,8 @@ ball.penup()
 ball.goto(0, 0)
 
 # Ball speed
-ball.dx = 0.15
-ball.dy = 0.15
+ball.dx = 0.9
+ball.dy = 0.9
 
 # Pen
 pen = turtle.Turtle()
@@ -112,23 +112,23 @@ while True:
         if ball.xcor() > 350:
             score_a += 1
             pen.clear()
-            pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+            pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
             ball.goto(0, 0)
             ball.dx *= -1
 
         elif ball.xcor() < -350:
             score_b += 1
             pen.clear()
-            pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+            pen.write(f"Player A: {score_a}  Player B: {score_a}", align="center", font=("Courier", 24, "normal"))
             ball.goto(0, 0)
             ball.dx *= -1
 
         # Paddle and ball collisions
-        if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        if ball.xcor() < -340 and paddle_a.ycor() + 50 > ball.ycor() > paddle_a.ycor() - 50:
             ball.dx *= -1
             os.system("afplay bounce.wav&")
 
-        elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        elif ball.xcor() > 340 and paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50:
             ball.dx *= -1
             os.system("afplay bounce.wav&")
     except:
